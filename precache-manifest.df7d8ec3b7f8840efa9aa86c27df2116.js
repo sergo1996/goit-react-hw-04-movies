532d8dea3b2bdc72d48b41ac3b0842daf3bc04d1
sergo1996 <56196384+sergo1996@users.noreply.github.com>
2020-09-30 21:35:05 +0300

    Updates

---
+++ precache-manifest.df7d8ec3b7f8840efa9aa86c27df2116.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db61cc0d7037c8354faba06786f15ed",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "1ea7e706ef663ce9b96e",
+    "url": "/goit-react-hw-04-movies/static/css/main.f5f9807c.chunk.css"
+  },
+  {
+    "revision": "3cfe6e3c2a5a98c61dda",
+    "url": "/goit-react-hw-04-movies/static/js/4.4aea9fdd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-04-movies/static/js/4.4aea9fdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ea7e706ef663ce9b96e",
+    "url": "/goit-react-hw-04-movies/static/js/main.86e9a9ed.chunk.js"
+  },
+  {
+    "revision": "12bddb8098e728062a09",
+    "url": "/goit-react-hw-04-movies/static/js/movie_cast.c0907a60.chunk.js"
+  },
+  {
+    "revision": "739bffb495e54fcb15d9",
+    "url": "/goit-react-hw-04-movies/static/js/movie_reviews.8b197485.chunk.js"
+  },
+  {
+    "revision": "5c383f7cf13855e9b81f",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.2a93017f.js"
+  }
+]);
